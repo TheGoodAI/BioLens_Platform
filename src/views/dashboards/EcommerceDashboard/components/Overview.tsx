f447be119fd671f8c8b5568e238b5baed597f6fc
BinaryNova <saksham@thegoodai.ai>
2026-04-18 14:02:32 +0530

    fix: imports case sensitive in other files

--- src/views/dashboards/EcommerceDashboard/components/Overview.tsx
+++ src/views/dashboards/EcommerceDashboard/components/Overview.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card } from '@/components/ui/card';
+import { Card } from '@/components/ui/Card';
 import { HeartPulse, Dna, Activity } from 'lucide-react';
 import {
   LineChart,
